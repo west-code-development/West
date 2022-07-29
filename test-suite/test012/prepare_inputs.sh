e7f8f5a213809895065929d9a859574f1e4d26a5
Christian Vorwerk <vorwerk@uchicago.edu>
2022-07-29 09:59:25 -0500

    Add test for QDET one-body terms.

---
+++ test-suite/test012/prepare_inputs.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+${WGET} http://www.quantum-simulation.org/potentials/sg15_oncv/upf/Mg_ONCV_PBE-1.2.upf
+${WGET} http://www.quantum-simulation.org/potentials/sg15_oncv/upf/O_ONCV_PBE-1.2.upf
+
+cat > pw.in << EOF
+&CONTROL                                                                        
+  calculation = 'scf',                                                          
+  pseudo_dir = './'                                                             
+  wf_collect = .TRUE.                                                           
+/                                                                               
+&SYSTEM                                                                         
+  ibrav = 0,                                                                    
+  nat = 15,                                                                     
+  ntyp = 2,                                                                     
+  input_dft = 'PBE'                                                             
+  ecutwfc = 80                                                                  
+  nspin = 1                                                                     
+  nbnd = 80                                                                     
+/                                                                               
+&ELECTRONS                                                                      
+/                                                                               
+&IONS                                                                           
+/                                                                               
+&CELL                                                                           
+/                                                                               
+ATOMIC_SPECIES                                                                  
+  Mg  24.3050 Mg_ONCV_PBE-1.2.upf                                               
+  O  15.9994 O_ONCV_PBE-1.2.upf                                                 
+ATOMIC_POSITIONS crystal                                                        
+Mg      -0.000000000   0.000000000  -0.000000000                                
+Mg      -0.002103461  -0.002103461   0.502103461                                
+Mg      -0.002103461   0.502103461  -0.002103461                                
+Mg      -0.002103461   0.502103461   0.502103461                                
+Mg       0.502103461  -0.002103461  -0.002103461                                
+Mg       0.502103461  -0.002103461   0.502103461                                
+Mg       0.502103461   0.502103461  -0.002103461                                
+Mg       0.500000000   0.500000000   0.500000000                                
+O        0.250000000   0.250000000   0.750000000                                
+O        0.250000000   0.750000000   0.250000000                                
+O        0.250000000   0.750000000   0.750000000                                
+O        0.750000000   0.250000000   0.250000000                                
+O        0.750000000   0.250000000   0.750000000                                
+O        0.750000000   0.750000000   0.250000000                                
+O        0.750000000   0.750000000   0.750000000                                
+K_POINTS gamma                                                                  
+CELL_PARAMETERS angstrom                                                        
+  -0.000000 4.249236 4.249236                                                   
+  4.249236 0.000000 4.249236                                                    
+  4.249236 4.249236 0.000000
+EOF
+
+
+cat > wstat.in << EOF
+{                                                                               
+  "input_west": {},                                                             
+  "wstat_control": {                                                            
+    "wstat_calculation": "S",                                                   
+    "l_minimize_exx_if_active": true,                                           
+    "n_pdep_eigen": 61,                                                         
+    "trev_pdep": 0.01                                                           
+  }                                                                             
+}      
+EOF
+
+
+cat > wfreq.in << EOF
+wstat_control:                                                                  
+  wstat_calculation: S                                                          
+  l_minimize_exx_if_active: true                                                
+  n_pdep_eigen: 61                                                              
+  trev_pdep: 0.01                                                               
+                                                                                
+wfreq_control:                                                                  
+  qp_bands: [61, 63, 64, 65]                                                    
+  wfreq_calculation: XWGQH                                                      
+  n_pdep_eigen_to_use: 61                                                       
+  l_enable_lanczos: true                                                        
+  l_enable_off_diagonal: true                                                   
+  macropol_calculation: C
+EOF
---
+++ test-suite/test012/test_qdet.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import json 
+import os 
+import pytest
+
+def read_h1e_from_json(filename):
+    """
+    Read QDET one-body terms from JSON file.
+    """
+    with open(filename, 'r') as f:
+        raw_ = json.load(f)
+    
+    return np.array(raw_['qdet']['h1e']['K000001'], dtype=float)
+
+def test_h1e():     
+    """     
+    Test whether matrix elements of the screened Coulomb potential are correct.     
+    """     
+    # get parameters from JSON file     
+    with open('./parameters.json', 'r') as f:         
+        parameters = json.load(f)     
+    
+    ref_h1e = read_h1e_from_json('./test012/ref/wfreq.json')     
+    test_h1e = read_h1e_from_json('./test012/west.wfreq.save/wfreq.json')     
+    
+    np.testing.assert_almost_equal(ref_h1e, test_h1e,
+            decimal=np.log10(float(parameters['tolerance']['pdep_eigenvalue'])))

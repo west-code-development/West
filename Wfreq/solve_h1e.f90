6184127fc28934be8ab212681298827009e32865
Christian Vorwerk <vorwerk@uchicago.edu>
2022-08-29 12:17:04 -0500

    Simplify calculation of one-body terms.

--- Wfreq/solve_h1e.f90
+++ Wfreq/solve_h1e.f90
@@ -69,26 +69,17 @@ SUBROUTINE solve_h1e()
       h1e(ijpmap(iband, iband),s) = et(qp_bands(iband), s)  
     ENDDO
   ENDDO 
-  !
-  CALL compute_vxc(psi, hpsi, h1e_tmp)
-  ! H1e = H^{KS} - V_{xc}
-  h1e = h1e - h1e_tmp
-  !
-  CALL compute_vxx(psi, hpsi, h1e_tmp)
-  ! H1e = H^{KS} - V_{xc} - V_{xx}
-  h1e = h1e - h1e_tmp
+  ! H1e = H^{KS} + \Sigma^{x} + \Sigma^{c}
+  !     = H^{KS} + (\Sigma^{x} - V_{xc} - V_{xx} ) + \Sigma^{c}
+  h1e = h1e + sigma_hf_full + REAL(sigma_corr_full)
   !
   CALL compute_hartree_double_counting(h1e_tmp)
   ! H1e = H^{KS} - V_{xc} - V_{xx} - V^{H}_{dc}
   h1e = h1e - h1e_tmp
-  ! H1e = H^{KS} - V_{xc} - V_{xx} - V^{H}_{dc} + \Sigma^{x}
-  h1e = h1e + sigma_exx_full
   !
   CALL calc_exx2(sigma_exx, .TRUE.)
   ! H1e = H^{KS} - V_{xc} - V_{xx} + \Sigma^{x} - \Sigma^{x}_{dc}
   h1e = h1e - sigma_exx_full
-  ! H1e = H^{KS} - V_{xc} - V_{xx} - V^{H}_{dc} + \Sigma^{x} - \Sigma^{x}_{dc} + \Sigma^{c}
-  h1e = h1e + REAL(sigma_corr_full)
   ! Call to solve_qp with
   ! l_secant = .FALSE.
   ! l_generate_plot = .FALSE.
@@ -103,161 +94,6 @@ SUBROUTINE solve_h1e()
   !
   DEALLOCATE( psi, hpsi )
   DEALLOCATE( h1e, h1e_tmp )
-  !
-END SUBROUTINE
-!
-!-----------------------------------------------------------------------  
-SUBROUTINE compute_vxc(psi, hpsi, h1e_tmp)
-  !-----------------------------------------------------------------------
-  !
-  USE kinds,                ONLY : DP
-  USE pwcom,                ONLY : nspin,npw,npwx
-  USE westcom,              ONLY : n_bands,n_pairs
-  USE scf,                  ONLY : scf_type,rho,rho_core,rhog_core, &
-                                 & create_scf_type,destroy_scf_type
-  USE xc_lib,               ONLY : exx_is_active
-  USE exx,                  ONLY : vexx,vexxace_gamma,use_ace
-  USE wvfct,                ONLY : current_k
-  USE lsda_mod,             ONLY : current_spin
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(IN)  :: psi(npwx,n_bands,nspin)
-  COMPLEX(DP), INTENT(OUT) :: hpsi(npwx,n_bands,nspin)
-  REAL(DP), INTENT(OUT)    :: h1e_tmp(n_pairs,nspin)
-  INTEGER  :: i, s
-  TYPE(scf_type) :: vhxc
-  REAL(DP) :: ehart, charge, etxc, vtxc, eth, etotefield, ee
-  !
-  ! Computes Matrix elements of V_{xc} in the pair-basis.
-  !
-  CALL create_scf_type(vhxc, do_not_allocate_becsum = .true.)
-  !
-  ! XC (semilocal)
-  !
-  hpsi = 0._DP
-  vhxc%of_r = 0._DP
-  !
-  CALL v_xc(rho, rho_core, rhog_core, etxc, vtxc, vhxc%of_r)
-  !
-  DO s = 1, nspin
-    !
-    CALL vloc_psi_gamma(npwx, npw, n_bands, psi(:,:,s), vhxc%of_r(:,s), hpsi(:,:,s))
-    !
-  ENDDO
-  !
-  CALL compute_integrals(psi, hpsi, h1e_tmp)
-  !
-  CALL destroy_scf_type(vhxc)
-  !
-END SUBROUTINE
-!
-!-----------------------------------------------------------------------  
-SUBROUTINE compute_vxx(psi, hpsi, h1e_tmp)
-!-----------------------------------------------------------------------
-  !
-  USE kinds,                ONLY : DP
-  USE pwcom,                ONLY : nspin,npw,npwx
-  USE westcom,              ONLY : n_bands,n_pairs
-  USE scf,                  ONLY : scf_type,rho,rho_core,rhog_core, &
-                                 & create_scf_type,destroy_scf_type
-  USE xc_lib,               ONLY : exx_is_active
-  USE exx,                  ONLY : vexx,vexxace_gamma,use_ace
-  USE wvfct,                ONLY : current_k
-  USE lsda_mod,             ONLY : current_spin
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(IN)  :: psi(npwx,n_bands,nspin)
-  COMPLEX(DP), INTENT(OUT) :: hpsi(npwx,n_bands,nspin)
-  REAL(DP), INTENT(OUT)    :: h1e_tmp(n_pairs,nspin)
-  INTEGER  :: i, s
-  TYPE(scf_type) :: vhxc
-  REAL(DP) :: ehart, charge, etxc, vtxc, eth, etotefield, ee
-  !
-  CALL create_scf_type(vhxc, do_not_allocate_becsum = .true.)
-  !
-  ! EXX
-  !
-  hpsi = 0._DP
-  !
-  IF ( exx_is_active() ) THEN
-     !
-     DO s = 1, nspin
-        !
-        current_k = s
-        current_spin = s
-        !
-        IF ( use_ace) THEN
-           CALL vexxace_gamma(npwx, n_bands, psi(:,:,s), ee, hpsi(:,:,s))
-        ELSE
-           CALL vexx(npwx, npw, n_bands, psi(:,:,s), hpsi(:,:,s))
-        END IF
-     END DO
-     !
-  END IF
-  !
-  CALL compute_integrals(psi, hpsi, h1e_tmp)
-  !
-  CALL destroy_scf_type(vhxc)
-  !
-END SUBROUTINE
-!
-!-----------------------------------------------------------------------  
-SUBROUTINE compute_integrals(psi, hpsi, h1e)
-  !-----------------------------------------------------------------------
-  !
-  USE kinds,                ONLY : DP
-  USE pwcom,                ONLY : nspin,npw,npwx
-  USE westcom,              ONLY : n_bands,n_pairs,ijpmap,wfreq_save_dir
-  USE mp_global,            ONLY : intra_bgrp_comm
-  USE mp,                   ONLY : mp_sum
-  USE mp_world,             ONLY : mpime,root  
-  USE gvect,                ONLY : gstart
-  USE noncollin_module,     ONLY : npol
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(IN)  :: psi(npwx,n_bands,nspin)
-  COMPLEX(DP), INTENT(OUT) :: hpsi(npwx,n_bands,nspin)
-  REAL(DP), INTENT(OUT)    :: h1e(n_pairs,nspin)
-  !
-  REAL(DP),ALLOCATABLE     :: h1e_tmp(:,:,:)
-  INTEGER   :: i, j, s, ib_index, jb_Index
-  INTEGER   :: iunit
-  REAL(DP)  :: ry_to_ha = 0.5_DP
-  REAL(DP), EXTERNAL    :: DDOT
-  !
-  ALLOCATE( h1e_tmp(n_bands,n_bands,nspin) )
-  h1e_tmp = 0._DP
-  !
-  ! Following h_psi subroutine, set imaginary part of G=0 component of hpsi to zero
-  !
-  IF ( gstart == 2 ) THEN
-     hpsi(1,:,:) = CMPLX( DBLE( hpsi(1,:,:) ), 0.D0 ,kind=DP)
-  ENDIF
-  !
-  DO s = 1, nspin
-     !
-     ! Following regterg subroutine, compute overlap integrals between psi and hpsi
-     ! Hamiltonian matrix (or its components) is assumed to be real and symmetric
-     !
-     CALL glbrak_gamma( psi(:,:,s), hpsi(:,:,s), h1e_tmp(:,:,s), npw, npwx, n_bands, n_bands, n_bands, npol)
-     !
-  ENDDO
-  !
-  CALL mp_sum(h1e_tmp, intra_bgrp_comm)
-  !
-  DO s = 1, nspin    
-    DO ib_index = 1, n_bands
-       DO jb_index = 1, n_bands
-          IF (ib_index > jb_index) CYCLE
-          h1e(ijpmap(ib_index,jb_index),s) = h1e_tmp(ib_index,jb_index,s)
-       ENDDO
-    ENDDO
-  ENDDO
-  !
-  DEALLOCATE(h1e_tmp)
   !
 END SUBROUTINE
 !

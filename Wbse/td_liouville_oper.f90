8cabb12009c7874d3b83519f8cc184942fb3a247
Victor Yu <victor.wz.yu.1012@gmail.com>
2022-11-26 10:40:26 -0600

    OpenACC td_liouville_oper

--- Wbse/td_liouville_oper.f90
+++ Wbse/td_liouville_oper.f90
@@ -5,7 +5,7 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
+SUBROUTINE west_apply_liouvillian(evc1,evc1_new)
   !
   ! Applies the linear response operator to response wavefunctions
   !
@@ -14,11 +14,10 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
   USE gvect,                ONLY : gstart
   USE uspp,                 ONLY : vkb,nkb
   USE lsda_mod,             ONLY : nspin
-  USE wavefunctions,        ONLY : psic,evc
   USE pwcom,                ONLY : npw,npwx,et,current_k,current_spin,isk,lsda,nks,xk,ngk,igk_k
   USE control_flags,        ONLY : gamma_only
   USE mp,                   ONLY : mp_sum,mp_barrier,mp_bcast
-  USE mp_global,            ONLY : my_image_id,inter_image_comm,inter_bgrp_comm
+  USE mp_global,            ONLY : nimage,my_image_id,inter_image_comm,nbgrp,my_bgrp_id,inter_bgrp_comm
   USE noncollin_module,     ONLY : npol
   USE buffers,              ONLY : get_buffer
   USE fft_at_gamma,         ONLY : single_fwfft_gamma,single_invfft_gamma,double_fwfft_gamma,&
@@ -30,6 +29,15 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
   USE distribution_center,  ONLY : aband
   USE uspp_init,            ONLY : init_us_2
   USE wbse_dv,              ONLY : wbse_dv_of_drho
+#if defined(__CUDA)
+  USE wavefunctions_gpum,   ONLY : using_evc,using_evc_d,evc_work=>evc_d,psic=>psic_d
+  USE wavefunctions,        ONLY : evc_host=>evc
+  USE wvfct_gpum,           ONLY : using_et,using_et_d
+  USE becmod_subs_gpum,     ONLY : using_becp_auto,using_becp_d_auto
+  USE cublas
+#else
+  USE wavefunctions,        ONLY : evc_work=>evc,psic
+#endif
   !
   IMPLICIT NONE
   !
@@ -38,37 +46,60 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
   !
   ! Local variables
   !
-  INTEGER :: ibnd, ibnd_1, iks, ir, nbndval
-  INTEGER :: nbvalloc, il1
+  INTEGER :: ibnd,jbnd,iks,ir,ig,nbndval,nbvalloc,il1
+  INTEGER :: dffts_nnr
+  COMPLEX(DP) :: factor
   COMPLEX(DP), ALLOCATABLE :: dvrs(:,:)
   COMPLEX(DP), ALLOCATABLE :: hevc1(:,:)
   COMPLEX(DP), ALLOCATABLE :: evc1_aux(:,:)
+  !$acc declare device_resident(hevc1,evc1_aux)
   !
+#if defined(__CUDA)
+  CALL start_clock_gpu('apply_lv')
+#else
   CALL start_clock('apply_lv')
+#endif
   !
+  dffts_nnr = dffts%nnr
+  !
+  !$acc kernels present(evc1_new)
   evc1_new(:,:,:) = (0._DP,0._DP)
+  !$acc end kernels
   !
   ALLOCATE(dvrs(dfftp%nnr,nspin))
+  !$acc enter data create(dvrs)
+  !
+  nbndval = MAXVAL(nbnd_occ)
+  nbvalloc = 0
+  DO il1 = 1,aband%nloc
+     ibnd = aband%l2g(il1)
+     IF(ibnd < 1 .OR. ibnd > nbndval) CYCLE
+     nbvalloc = nbvalloc+1
+  ENDDO
+  !
+  ALLOCATE(hevc1(npwx*npol,nbvalloc))
+  ALLOCATE(evc1_aux(npwx*npol,nbvalloc))
   !
   ! Calculation of the charge density response
   !
-  CALL wbse_calc_dens(evc1, dvrs)
+  CALL wbse_calc_dens(evc1,dvrs)
   !
   IF(l_bse_calculation) THEN
-     CALL wbse_dv_of_drho(dvrs, .TRUE., .FALSE.)
+     CALL wbse_dv_of_drho(dvrs,.TRUE.,.FALSE.)
   ELSE
-     CALL wbse_dv_of_drho(dvrs, .FALSE., .FALSE.)
+     CALL wbse_dv_of_drho(dvrs,.FALSE.,.FALSE.)
   ENDIF
   !
-  DO iks = 1, nks
+  !$acc update device(dvrs)
+  !
+  DO iks = 1,nks
      !
      nbndval = nbnd_occ(iks)
-     !
      nbvalloc = 0
-     DO il1 = 1, aband%nloc
+     DO il1 = 1,aband%nloc
         ibnd = aband%l2g(il1)
         IF(ibnd < 1 .OR. ibnd > nbndval) CYCLE
-        nbvalloc = nbvalloc + 1
+        nbvalloc = nbvalloc+1
      ENDDO
      !
      ! ... Set k-point, spin, kinetic energy, needed by Hpsi
@@ -76,11 +107,20 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
      current_k = iks
      IF(lsda) current_spin = isk(iks)
      !
+#if defined(__CUDA)
+     CALL g2_kin_gpu(iks)
+     !
+     ! ... More stuff needed by the hamiltonian: nonlocal projectors
+     !
+     IF(nkb > 0) CALL init_us_2(ngk(iks),igk_k(1,iks),xk(1,iks),vkb,.TRUE.)
+#else
      CALL g2_kin(iks)
      !
      ! ... More stuff needed by the hamiltonian: nonlocal projectors
      !
-     IF(nkb > 0) CALL init_us_2(ngk(iks), igk_k(1,iks), xk(1,iks), vkb)
+     IF(nkb > 0) CALL init_us_2(ngk(iks),igk_k(1,iks),xk(1,iks),vkb,.FALSE.)
+#endif
+     CALL g2_kin(iks)
      !
      ! ... Number of G vectors for PW expansion of wfs at k
      !
@@ -89,9 +129,26 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
      ! ... read in GS wavefunctions iks
      !
      IF(nks > 1) THEN
-        IF(my_image_id == 0) CALL get_buffer(evc,lrwfc,iuwfc,iks)
-        CALL mp_bcast(evc,0,inter_image_comm)
+#if defined(__CUDA)
+        IF(my_image_id == 0) CALL get_buffer(evc_host,lrwfc,iuwfc,iks)
+        CALL mp_bcast(evc_host,0,inter_image_comm)
+#else
+        IF(my_image_id == 0) CALL get_buffer(evc_work,lrwfc,iuwfc,iks)
+        CALL mp_bcast(evc_work,0,inter_image_comm)
+#endif
      ENDIF
+     !
+#if defined(__CUDA)
+     !
+     ! ... Sync GPU
+     !
+     CALL using_becp_auto(2)
+     CALL using_becp_d_auto(0)
+     CALL using_evc(2)
+     CALL using_evc_d(0)
+     CALL using_et(2)
+     CALL using_et_d(0)
+#endif
      !
      IF(.NOT. l_bse_triplet) THEN
         !
@@ -99,17 +156,21 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
            !
            ! double bands @ gamma
            !
-           DO il1 = 1, nbvalloc - MOD(nbvalloc,2), 2
-              ibnd   = aband%l2g(il1)
-              ibnd_1 = aband%l2g(il1+1)
+           DO il1 = 1,nbvalloc-MOD(nbvalloc,2),2
+              ibnd = aband%l2g(il1)
+              jbnd = aband%l2g(il1+1)
               !
-              CALL double_invfft_gamma(dffts,npw,npwx,evc(:,ibnd),evc(:,ibnd_1),psic,'Wave')
+              CALL double_invfft_gamma(dffts,npw,npwx,evc_work(:,ibnd),evc_work(:,jbnd),psic,'Wave')
               !
-              DO ir = 1,dffts%nnr
-                 psic(ir) = psic(ir) * CMPLX(REAL(dvrs(ir,current_spin),KIND=DP),KIND=DP)
+              !$acc parallel loop present(dvrs)
+              DO ir = 1,dffts_nnr
+                 psic(ir) = psic(ir)*CMPLX(REAL(dvrs(ir,current_spin),KIND=DP),KIND=DP)
               ENDDO
+              !$acc end parallel
               !
-              CALL double_fwfft_gamma(dffts,npw,npwx,psic,evc1_new(:,ibnd,iks),evc1_new(:,ibnd_1,iks),'Wave')
+              !$acc host_data use_device(evc1_new)
+              CALL double_fwfft_gamma(dffts,npw,npwx,psic,evc1_new(:,ibnd,iks),evc1_new(:,jbnd,iks),'Wave')
+              !$acc end host_data
            ENDDO
            !
            ! single band @ gamma
@@ -117,42 +178,54 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
            IF(MOD(nbvalloc,2) == 1) THEN
               ibnd = aband%l2g(nbvalloc)
               !
-              CALL single_invfft_gamma(dffts,npw,npwx,evc(:,ibnd),psic,'Wave')
+              CALL single_invfft_gamma(dffts,npw,npwx,evc_work(:,ibnd),psic,'Wave')
               !
-              DO ir = 1,dffts%nnr
+              !$acc parallel loop present(dvrs)
+              DO ir = 1,dffts_nnr
                  psic(ir) = CMPLX(REAL(psic(ir),KIND=DP)*REAL(dvrs(ir,current_spin),KIND=DP),KIND=DP)
               ENDDO
+              !$acc end parallel
               !
+              !$acc host_data use_device(evc1_new)
               CALL single_fwfft_gamma(dffts,npw,npwx,psic,evc1_new(:,ibnd,iks),'Wave')
+              !$acc end host_data
            ENDIF
            !
         ELSE
            !
            ! only single bands
            !
-           DO il1 = 1, nbvalloc
+           DO il1 = 1,nbvalloc
               ibnd = aband%l2g(il1)
               !
-              CALL single_invfft_k(dffts,npw,npwx,evc(:,ibnd),psic,'Wave',igk_k(:,current_k))
+              CALL single_invfft_k(dffts,npw,npwx,evc_work(:,ibnd),psic,'Wave',igk_k(:,current_k))
               !
-              DO ir = 1, dffts%nnr
-                 psic(ir) = psic(ir) * dvrs(ir,current_spin)
+              !$acc parallel loop present(dvrs)
+              DO ir = 1,dffts_nnr
+                 psic(ir) = psic(ir)*dvrs(ir,current_spin)
               ENDDO
+              !$acc end parallel
               !
+              !$acc host_data use_device(evc1_new)
               CALL single_fwfft_k(dffts,npw,npwx,psic,evc1_new(:,ibnd,iks),'Wave',igk_k(:,current_k))
+              !$acc end host_data
            ENDDO
            !
            IF(npol == 2) THEN
-              DO il1 = 1, nbvalloc
+              DO il1 = 1,nbvalloc
                  ibnd = aband%l2g(il1)
                  !
-                 CALL single_invfft_k(dffts,npw,npwx,evc(npwx+1:npwx*2,ibnd),psic,'Wave',igk_k(:,current_k))
+                 CALL single_invfft_k(dffts,npw,npwx,evc_work(npwx+1:npwx*2,ibnd),psic,'Wave',igk_k(:,current_k))
                  !
-                 DO ir = 1, dffts%nnr
-                    psic(ir) = psic(ir) * dvrs(ir,current_spin)
+                 !$acc parallel loop present(dvrs)
+                 DO ir = 1,dffts_nnr
+                    psic(ir) = psic(ir)*dvrs(ir,current_spin)
                  ENDDO
+                 !$acc end parallel
                  !
+                 !$acc host_data use_device(evc1_new)
                  CALL single_fwfft_k(dffts,npw,npwx,psic,evc1_new(npwx+1:npwx*2,ibnd,iks),'Wave',igk_k(:,current_k))
+                 !$acc end host_data
               ENDDO
            ENDIF
            !
@@ -160,71 +233,100 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
         !
      ENDIF
      !
-     ALLOCATE(hevc1(npwx*npol,nbvalloc))
-     ALLOCATE(evc1_aux(npwx*npol,nbvalloc))
-     !
-     evc1_aux(:,:) = (0._DP,0._DP)
-     !
-     DO il1 = 1, nbvalloc
-        ibnd = aband%l2g(il1)
-        evc1_aux(:,il1) = evc1(:,ibnd,iks)
+     !$acc parallel loop collapse(2) present(evc1_aux,evc1)
+     DO il1 = 1,nbvalloc
+        !
+        ! ibnd = aband%l2g(il1)
+        !
+        DO ig = 1,npw
+           IF(l_lanczos) THEN
+              ibnd = nimage*(il1-1)+my_image_id+1
+           ELSE
+              ibnd = nbgrp*(il1-1)+my_bgrp_id+1
+           ENDIF
+           evc1_aux(ig,il1) = evc1(ig,ibnd,iks)
+        ENDDO
+        !
      ENDDO
+     !$acc end parallel
      !
-     CALL h_psi(npwx,npw,nbvalloc,evc1_aux,hevc1)
+     ! use h_psi_, i.e. h_psi without band parallelization, as west
+     ! handles band parallelization separately
+     !
+#if defined(__CUDA)
+     !$acc host_data use_device(evc1_aux,hevc1)
+     CALL h_psi__gpu(npwx,npw,nbvalloc,evc1_aux,hevc1)
+     !$acc end host_data
+#else
+     CALL h_psi_(npwx,npw,nbvalloc,evc1_aux,hevc1)
+#endif
      !
      IF(l_qp_correction) THEN
         CALL apply_hqp_to_m_wfcs(iks,nbvalloc,evc1_aux,hevc1)
      ENDIF
      !
-     DEALLOCATE(evc1_aux)
-     !
      ! Subtract the eigenvalues
      !
-     IF(l_qp_correction) THEN
-        DO il1 = 1, nbvalloc
-           ibnd = aband%l2g(il1)
-           !
-           IF(l_bse_calculation) THEN
-              CALL ZAXPY(npw, CMPLX(-(et_qp(ibnd,iks)-scissor_ope+sigma_x_head+sigma_c_head),KIND=DP), &
-                   & evc1(:,ibnd,iks), 1, hevc1(:,il1), 1)
-           ELSE
-              CALL ZAXPY(npw, CMPLX(-(et_qp(ibnd,iks)-scissor_ope),KIND=DP), evc1(:,ibnd,iks), 1, hevc1(:,il1), 1)
-              !
-           ENDIF
-        ENDDO
-     ELSE
-        DO il1 = 1, nbvalloc
-           ibnd = aband%l2g(il1)
-           !
-           IF(l_bse_calculation) THEN
-              CALL ZAXPY(npw, CMPLX(-(et(ibnd,iks)-scissor_ope+sigma_x_head+sigma_c_head),KIND=DP), &
-                   & evc1(:,ibnd,iks), 1, hevc1(:,il1), 1)
-           ELSE
-              CALL ZAXPY(npw, CMPLX(-(et(ibnd,iks)-scissor_ope),KIND=DP), evc1(:,ibnd,iks), 1, hevc1(:,il1), 1)
-           ENDIF
-        ENDDO
-     ENDIF
-     !
-     DO il1 = 1, nbvalloc
+     DO il1 = 1,nbvalloc
+        !
         ibnd = aband%l2g(il1)
-        evc1_new(:,ibnd,iks) = hevc1(:,il1) + evc1_new(:,ibnd,iks)
+        !
+        IF(l_qp_correction) THEN
+           IF(l_bse_calculation) THEN
+              factor = CMPLX(-(et_qp(ibnd,iks)-scissor_ope+sigma_x_head+sigma_c_head),KIND=DP)
+           ELSE
+              factor = CMPLX(-(et_qp(ibnd,iks)-scissor_ope),KIND=DP)
+           ENDIF
+        ELSE
+           IF(l_bse_calculation) THEN
+              factor = CMPLX(-(et(ibnd,iks)-scissor_ope+sigma_x_head+sigma_c_head),KIND=DP)
+           ELSE
+              factor = CMPLX(-(et(ibnd,iks)-scissor_ope),KIND=DP)
+           ENDIF
+        ENDIF
+        !
+        !$acc host_data use_device(evc1,hevc1)
+        CALL ZAXPY(npw,factor,evc1(:,ibnd,iks),1,hevc1(:,il1),1)
+        !$acc end host_data
+        !
      ENDDO
      !
-     DEALLOCATE(hevc1)
+     !$acc parallel loop collapse(2) present(evc1_new,hevc1)
+     DO il1 = 1,nbvalloc
+        !
+        ! ibnd = aband%l2g(il1)
+        !
+        DO ig = 1,npw
+           IF(l_lanczos) THEN
+              ibnd = nimage*(il1-1)+my_image_id+1
+           ELSE
+              ibnd = nbgrp*(il1-1)+my_bgrp_id+1
+           ENDIF
+           evc1_new(ig,ibnd,iks) = evc1_new(ig,ibnd,iks)+hevc1(ig,il1)
+        ENDDO
+        !
+     ENDDO
+     !$acc end parallel
      !
+     !$acc host_data use_device(evc1_new)
      IF(l_lanczos) THEN
         CALL mp_sum(evc1_new(:,:,iks),inter_image_comm)
      ELSE
         CALL mp_sum(evc1_new(:,:,iks),inter_bgrp_comm)
      ENDIF
+     !$acc end host_data
      !
      IF(l_bse_calculation) THEN
-        CALL wbse_bse_kernel(current_spin, nbndval, evc1, evc1_new(:,:,iks))
+        CALL wbse_bse_kernel(current_spin,nbndval,evc1,evc1_new(:,:,iks))
      ENDIF
      !
      IF(gamma_only) THEN
         IF(gstart == 2) THEN
-           evc1_new(1,:,iks) = CMPLX(REAL(evc1_new(1,:,iks),KIND=DP),KIND=DP)
+           !$acc parallel loop present(evc1_new)
+           DO ibnd = 1,nbndval0x
+              evc1_new(1,ibnd,iks) = CMPLX(REAL(evc1_new(1,ibnd,iks),KIND=DP),KIND=DP)
+           ENDDO
+           !$acc end parallel
         ENDIF
      ENDIF
      !
@@ -234,8 +336,15 @@ SUBROUTINE west_apply_liouvillian(evc1, evc1_new)
      !
   ENDDO
   !
+  !$acc exit data delete(dvrs)
   DEALLOCATE(dvrs)
+  DEALLOCATE(hevc1)
+  DEALLOCATE(evc1_aux)
   !
+#if defined(__CUDA)
+  CALL stop_clock_gpu('apply_lv')
+#else
   CALL stop_clock('apply_lv')
+#endif
   !
 END SUBROUTINE

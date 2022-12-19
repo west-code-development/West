cb7245854f15ec31d8cb01f32ea0dd7c982aff1e
Victor Yu <victor.wz.yu.1012@gmail.com>
2022-12-19 14:11:04 -0600

    Update copyright banner

--- Wbse/wbse_calc_dens.f90
+++ Wbse/wbse_calc_dens.f90
@@ -1,9 +1,14 @@
 !
-! Copyright (C) 2001-2016 Quantum ESPRESSO group
+! Copyright (C) 2015-2022 M. Govoni
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
+!
+! This file is part of WEST.
+!
+! Contributors to this file:
+! Marco Govoni
 !
 !---------------------------------------------------------------------
 SUBROUTINE wbse_calc_dens(devc, drho)

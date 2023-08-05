da91a3d65bce7c658d7d7ade3c02b85c066b271c
Victor Yu <victor.wz.yu.1012@gmail.com>
2023-08-04 19:36:59 -0500

    Reduce QDET memory
    Tha band (or band pair) index in multiple arrays can be distributed when using
    band groups. This is particularly useful in large QDET runs as the number of
    band pairs grows quadratically.

--- Wfreq/solve_qp.f90
+++ Wfreq/solve_qp.f90
@@ -48,7 +48,7 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
                                  & d_body1_ifr_full,d_body2_ifr_full,d_diago_full,z_body_rfr_full,&
                                  & sigma_sc_eks_full,sigma_sc_eqplin_full,sigma_corr_full
   USE mp_global,            ONLY : inter_image_comm,nimage,my_image_id,inter_pool_comm,my_pool_id,&
-                                 & inter_bgrp_comm,intra_bgrp_comm
+                                 & intra_bgrp_comm
   USE mp_world,             ONLY : mpime,root
   USE mp,                   ONLY : mp_sum,mp_bcast
   USE pwcom,                ONLY : et,nbnd
@@ -81,7 +81,7 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
   REAL(DP),ALLOCATABLE :: en(:,:,:)
   LOGICAL,ALLOCATABLE :: l_conv(:,:)
   REAL(DP),PARAMETER :: eshift = 0.007349862_DP ! = 0.1 eV
-  INTEGER :: ib,ibloc,ib_index,jb,jb_index,ipair,iks,iks_g
+  INTEGER :: ib,ibloc,ib_index,jb,jb_index,ipair,iloc_pair,nloc_pairs,iks,iks_g
   INTEGER :: ifixed,ip,ifreq,il,im,im_index,glob_im,glob_jp,glob_ifreq
   REAL(DP),ALLOCATABLE :: out_tab(:,:)
   CHARACTER(LEN=5) :: myglobk
@@ -119,6 +119,10 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
 #else
   CALL start_clock('solve_qp')
 #endif
+  !
+  CALL io_push_title('Collecting results from W and G')
+  !
+  CALL band_group%init(n_bands,'b','band_group',.FALSE.)
   !
   IF ( .NOT. l_QDET ) THEN
      ALLOCATE( imfreq_list_integrate( 2, ifr%nloc ) )
@@ -170,12 +174,23 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
 #endif
   ENDIF
   IF( l_enable_off_diagonal ) THEN
-     IF ( .NOT. l_QDET ) THEN
-        ALLOCATE( d_body1_ifr_full( aband%nloc, ifr%nloc, n_pairs, k_grid%nps ) )
-        ALLOCATE( z_body_rfr_full( aband%nloc, rfr%nloc, n_pairs, k_grid%nps ) )
+     !
+     nloc_pairs = 0
+     DO ibloc = 1, band_group%nloc
+        ib_index = band_group%l2g(ibloc)
+        ib = qp_bands(ib_index)
+        DO jb_index = 1, n_bands
+           jb = qp_bands(jb_index)
+           IF(jb <= ib) nloc_pairs = nloc_pairs+1
+        ENDDO
+     ENDDO
+     !
+     IF( .NOT. l_QDET ) THEN
+        ALLOCATE( d_body1_ifr_full( aband%nloc, ifr%nloc, nloc_pairs, k_grid%nps ) )
+        ALLOCATE( z_body_rfr_full( aband%nloc, rfr%nloc, nloc_pairs, k_grid%nps ) )
         IF( l_enable_lanczos ) THEN
-           ALLOCATE( d_diago_full( n_lanczos, pert%nloc, n_pairs, k_grid%nps ) )
-           ALLOCATE( d_body2_ifr_full( n_lanczos, pert%nloc, ifr%nloc, n_pairs, k_grid%nps ) )
+           ALLOCATE( d_diago_full( n_lanczos, pert%nloc, nloc_pairs, k_grid%nps ) )
+           ALLOCATE( d_body2_ifr_full( n_lanczos, pert%nloc, ifr%nloc, nloc_pairs, k_grid%nps ) )
         ENDIF
      ENDIF
      !
@@ -185,14 +200,15 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
         d_body2_ifr_full(:,:,:,:,:) = 0._DP
         d_diago_full(:,:,:,:) = 0._DP
      ENDIF
+     !
   ELSE
      !
-     IF ( .NOT. l_QDET ) THEN
-        ALLOCATE( d_body1_ifr( aband%nloc, ifr%nloc, n_bands, k_grid%nps ) )
-        ALLOCATE( z_body_rfr( aband%nloc, rfr%nloc, n_bands, k_grid%nps ) )
+     IF( .NOT. l_QDET ) THEN
+        ALLOCATE( d_body1_ifr( aband%nloc, ifr%nloc, band_group%nloc, k_grid%nps ) )
+        ALLOCATE( z_body_rfr( aband%nloc, rfr%nloc, band_group%nloc, k_grid%nps ) )
         IF( l_enable_lanczos ) THEN
-           ALLOCATE( d_diago( n_lanczos, pert%nloc, n_bands, k_grid%nps ) )
-           ALLOCATE( d_body2_ifr( n_lanczos, pert%nloc, ifr%nloc, n_bands, k_grid%nps ) )
+           ALLOCATE( d_diago( n_lanczos, pert%nloc, band_group%nloc, k_grid%nps ) )
+           ALLOCATE( d_body2_ifr( n_lanczos, pert%nloc, ifr%nloc, band_group%nloc, k_grid%nps ) )
         ENDIF
      ENDIF
      !
@@ -223,10 +239,6 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
   !
   ! d_body1_ifr, d_body2_ifr, z_diago_rfr, d_diago
   !
-  CALL io_push_title('Collecting results from W and G')
-  !
-  CALL band_group%init(n_bands,'b','band_group',.FALSE.)
-  !
   barra_load = 0
   DO ibloc = 1,band_group%nloc
      ib_index = band_group%l2g(ibloc)
@@ -246,6 +258,7 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
   DO iks = 1,kpt_pool%nloc ! KPOINT-SPIN
      !
      iks_g = kpt_pool%l2g(iks)
+     iloc_pair = 0
      !
      DO ibloc = 1, band_group%nloc
         !
@@ -271,6 +284,7 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
            IF(l_enable_off_diagonal .AND. jb <= ib) THEN
               !
               ipair = ijpmap(jb_index,ib_index)
+              iloc_pair = iloc_pair+1
               !
               CALL readin_overlap( 'g', kpt_pool%l2g(iks), jb, overlap, pert%nglob, nbnd )
               !
@@ -338,9 +352,9 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
               DO im = 1, aband%nloc
                  glob_im = aband%l2g(im)
                  IF(l_enable_off_diagonal .AND. jb <= ib) THEN
-                    d_body1_ifr_full(im,ifreq,ipair,iks_g) = dtemp2(glob_im,ifreq)
+                    d_body1_ifr_full(im,ifreq,iloc_pair,iks_g) = dtemp2(glob_im,ifreq)
                  ELSEIF(.NOT. l_enable_off_diagonal .AND. jb == ib) THEN
-                    d_body1_ifr(im,ifreq,ib_index,iks_g) = dtemp2(glob_im,ifreq)
+                    d_body1_ifr(im,ifreq,ibloc,iks_g) = dtemp2(glob_im,ifreq)
                  ENDIF
               ENDDO
            ENDDO
@@ -403,9 +417,9 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
               DO im = 1, aband%nloc
                  glob_im = aband%l2g(im)
                  IF(l_enable_off_diagonal .AND. jb <= ib) THEN
-                    z_body_rfr_full(im,ifreq,ipair,iks_g) = ztemp2(glob_im,ifreq)
+                    z_body_rfr_full(im,ifreq,iloc_pair,iks_g) = ztemp2(glob_im,ifreq)
                  ELSEIF(.NOT. l_enable_off_diagonal .AND. jb == ib) THEN
-                    z_body_rfr(im,ifreq,ib_index,iks_g) = ztemp2(glob_im,ifreq)
+                    z_body_rfr(im,ifreq,ibloc,iks_g) = ztemp2(glob_im,ifreq)
                  ENDIF
               ENDDO
            ENDDO
@@ -447,9 +461,9 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
               DO ip = 1, pert%nloc
                  DO il = 1, n_lanczos
                     IF(l_enable_off_diagonal .AND. jb <= ib) THEN
-                       d_diago_full(il,ip,ipair,iks_g) = diago(il,ip)
+                       d_diago_full(il,ip,iloc_pair,iks_g) = diago(il,ip)
                     ELSEIF(.NOT. l_enable_off_diagonal .AND. jb == ib) THEN
-                       d_diago(il,ip,ib_index,iks_g) = diago(il,ip)
+                       d_diago(il,ip,ibloc,iks_g) = diago(il,ip)
                     ENDIF
                  ENDDO
               ENDDO
@@ -468,9 +482,9 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
                        d_body2_ifr_tmp(il,ip,ifreq) = reduce_r
 #else
                        IF(l_enable_off_diagonal .AND. jb <= ib) THEN
-                          d_body2_ifr_full(il,ip,ifreq,ipair,iks_g) = reduce_r
+                          d_body2_ifr_full(il,ip,ifreq,iloc_pair,iks_g) = reduce_r
                        ELSE
-                          d_body2_ifr(il,ip,ifreq,ib_index,iks_g) = reduce_r
+                          d_body2_ifr(il,ip,ifreq,ibloc,iks_g) = reduce_r
                        ENDIF
 #endif
                     ENDDO
@@ -480,9 +494,9 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
               !
 #if defined(__CUDA)
               IF(l_enable_off_diagonal .AND. jb <= ib) THEN
-                 CALL memcpy_D2H(d_body2_ifr_full(:,:,:,ipair,iks_g),d_body2_ifr_tmp,n_lanczos*pert%nloc*ifr%nloc)
+                 CALL memcpy_D2H(d_body2_ifr_full(:,:,:,iloc_pair,iks_g),d_body2_ifr_tmp,n_lanczos*pert%nloc*ifr%nloc)
               ELSE
-                 CALL memcpy_D2H(d_body2_ifr(:,:,:,ib_index,iks_g),d_body2_ifr_tmp,n_lanczos*pert%nloc*ifr%nloc)
+                 CALL memcpy_D2H(d_body2_ifr(:,:,:,ibloc,iks_g),d_body2_ifr_tmp,n_lanczos*pert%nloc*ifr%nloc)
               ENDIF
 #endif
               !
@@ -497,25 +511,17 @@ SUBROUTINE solve_qp_gamma(l_secant,l_generate_plot,l_QDET)
   ENDDO ! iks
   !
   IF(l_enable_off_diagonal) THEN
-     CALL mp_sum(d_body1_ifr_full,inter_bgrp_comm)
      CALL mp_sum(d_body1_ifr_full,inter_pool_comm)
-     CALL mp_sum(z_body_rfr_full,inter_bgrp_comm)
      CALL mp_sum(z_body_rfr_full,inter_pool_comm)
      IF(l_enable_lanczos) THEN
-        CALL mp_sum(d_body2_ifr_full,inter_bgrp_comm)
         CALL mp_sum(d_body2_ifr_full,inter_pool_comm)
-        CALL mp_sum(d_diago_full,inter_bgrp_comm)
         CALL mp_sum(d_diago_full,inter_pool_comm)
      ENDIF
   ELSE
-     CALL mp_sum(d_body1_ifr,inter_bgrp_comm)
      CALL mp_sum(d_body1_ifr,inter_pool_comm)
-     CALL mp_sum(z_body_rfr,inter_bgrp_comm)
      CALL mp_sum(z_body_rfr,inter_pool_comm)
      IF(l_enable_lanczos) THEN
-        CALL mp_sum(d_body2_ifr,inter_bgrp_comm)
         CALL mp_sum(d_body2_ifr,inter_pool_comm)
-        CALL mp_sum(d_diago,inter_bgrp_comm)
         CALL mp_sum(d_diago,inter_pool_comm)
      ENDIF
   ENDIF
@@ -813,7 +819,7 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
                                  & z_body1_ifr_q,z_body2_ifr_q,d_diago_q,z_body_rfr_q,sigma_z,&
                                  & sigma_eqplin,sigma_eqpsec,sigma_sc_eks,sigma_sc_eqplin,&
                                  & sigma_sc_eqpsec,sigma_diff,sigma_spectralf,sigma_freq
-  USE mp_global,            ONLY : inter_image_comm,nimage,my_image_id,inter_bgrp_comm,intra_bgrp_comm
+  USE mp_global,            ONLY : inter_image_comm,nimage,my_image_id,intra_bgrp_comm
   USE mp_world,             ONLY : mpime,root
   USE mp,                   ONLY : mp_sum
   USE pwcom,                ONLY : et,nbnd
@@ -884,6 +890,10 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
   CALL start_clock('solve_qp')
 #endif
   !
+  CALL io_push_title('Collecting results from W and G')
+  !
+  CALL band_group%init(n_bands,'b','band_group',.FALSE.)
+  !
   ALLOCATE( imfreq_list_integrate( 2, ifr%nloc ) )
   ALLOCATE( dtemp( n_imfreq ) )
   !
@@ -916,13 +926,13 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
   !
   ALLOCATE( overlap( pert%nglob, nbnd ) )
   ALLOCATE( overlap_loc( pert%nloc,nbnd ) )
-  ALLOCATE( z_body1_ifr_q( aband%nloc, ifr%nloc, n_bands, k_grid%nps, q_grid%nps ) )
-  ALLOCATE( z_body_rfr_q( aband%nloc, rfr%nloc, n_bands, k_grid%nps, q_grid%nps ) )
+  ALLOCATE( z_body1_ifr_q( aband%nloc, ifr%nloc, band_group%nloc, k_grid%nps, q_grid%nps ) )
+  ALLOCATE( z_body_rfr_q( aband%nloc, rfr%nloc, band_group%nloc, k_grid%nps, q_grid%nps ) )
   ALLOCATE( ztemp2( nbnd, MAX(ifr%nloc,rfr%nloc) ) )
   !$acc enter data create(overlap,ztemp2) copyin(z_epsm1_ifr_q)
   IF( l_enable_lanczos ) THEN
-     ALLOCATE( z_body2_ifr_q( n_lanczos, pert%nloc, ifr%nloc, n_bands, k_grid%nps, q_grid%nps ) )
-     ALLOCATE( d_diago_q( n_lanczos, pert%nloc, n_bands, k_grid%nps, q_grid%nps ) )
+     ALLOCATE( z_body2_ifr_q( n_lanczos, pert%nloc, ifr%nloc, band_group%nloc, k_grid%nps, q_grid%nps ) )
+     ALLOCATE( d_diago_q( n_lanczos, pert%nloc, band_group%nloc, k_grid%nps, q_grid%nps ) )
      ALLOCATE( braket( pert%nglob, n_lanczos, pert%nloc ) )
      !$acc enter data create(braket)
      ALLOCATE( diago( n_lanczos, pert%nloc ) )
@@ -960,10 +970,6 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
   ENDIF
   !
   ! d_body1_ifr_q, d_body2_ifr_q, z_diago_rfr_q, d_diago_q
-  !
-  CALL io_push_title('Collecting results from W and G')
-  !
-  CALL band_group%init(n_bands,'b','band_group',.FALSE.)
   !
   barra_load = k_grid%nps * band_group%nloc * q_grid%nps
   CALL start_bar_type( barra, 'coll_gw', barra_load )
@@ -1032,7 +1038,7 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
            DO ifreq = 1, ifr%nloc
               DO im = 1, aband%nloc
                  glob_im = aband%l2g(im)
-                 z_body1_ifr_q(im,ifreq,ib_index,iks,iq) = ztemp2(glob_im,ifreq)
+                 z_body1_ifr_q(im,ifreq,ibloc,iks,iq) = ztemp2(glob_im,ifreq)
               ENDDO
            ENDDO
            !
@@ -1064,7 +1070,7 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
            DO ifreq = 1, rfr%nloc
               DO im = 1, aband%nloc
                  glob_im = aband%l2g(im)
-                 z_body_rfr_q(im,ifreq,ib_index,iks,iq) = ztemp2(glob_im,ifreq)
+                 z_body_rfr_q(im,ifreq,ibloc,iks,iq) = ztemp2(glob_im,ifreq)
               ENDDO
            ENDDO
            !
@@ -1081,7 +1087,7 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
               !
               DO ip = 1, pert%nloc
                  DO il = 1, n_lanczos
-                    d_diago_q(il,ip,ib_index,iks,iq) = diago(il,ip)
+                    d_diago_q(il,ip,ibloc,iks,iq) = diago(il,ip)
                  ENDDO
               ENDDO
               !
@@ -1098,7 +1104,7 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
 #if defined(__CUDA)
                        z_body2_ifr_q_tmp(il,ip,ifreq) = reduce
 #else
-                       z_body2_ifr_q(il,ip,ifreq,ib_index,iks,iq) = reduce
+                       z_body2_ifr_q(il,ip,ifreq,ibloc,iks,iq) = reduce
 #endif
                     ENDDO
                  ENDDO
@@ -1106,7 +1112,7 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
               !$acc end parallel
               !
 #if defined(__CUDA)
-              CALL memcpy_D2H(z_body2_ifr_q(:,:,:,ib_index,iks,iq),z_body2_ifr_q_tmp,n_lanczos*pert%nloc*ifr%nloc)
+              CALL memcpy_D2H(z_body2_ifr_q(:,:,:,ibloc,iks,iq),z_body2_ifr_q_tmp,n_lanczos*pert%nloc*ifr%nloc)
 #endif
               !
            ENDIF
@@ -1118,14 +1124,6 @@ SUBROUTINE solve_qp_k(l_secant,l_generate_plot)
      ENDDO ! ikks
      !
   ENDDO ! iks
-  !
-  CALL mp_sum(z_body1_ifr_q,inter_bgrp_comm)
-  CALL mp_sum(z_body_rfr_q,inter_bgrp_comm)
-  !
-  IF(l_enable_lanczos) THEN
-     CALL mp_sum(z_body2_ifr_q,inter_bgrp_comm)
-     CALL mp_sum(d_diago_q,inter_bgrp_comm)
-  ENDIF
   !
   CALL stop_bar_type( barra, 'coll_gw' )
   !

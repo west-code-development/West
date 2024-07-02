4782f1be5e1c57f0297a53cf5364522840ce972b
Victor Yu <victor.wz.yu.1012@gmail.com>
2024-07-02 16:08:40 -0500

    Add a test for excited state decomposition
    `westpp_calculation: C`

---
+++ test-suite/test028/test_exc_decomp.py
@@ -0,0 +1,41 @@
+import numpy as np
+import json
+
+
+def read_proj_matrix_from_json(fileName):
+    """
+    Read projection matrix from JSON file.
+    """
+
+    with open(fileName, "r") as f:
+        raw_ = json.load(f)
+
+    proj = {}
+    nexc = raw_["input"]["westpp_control"]["westpp_n_liouville_to_use"]
+
+    for iexc in range(nexc):
+        label = f"E{(iexc+1):06d}"
+        proj[label] = np.array(
+            raw_["output"][label]["K000001"]["projection"]["vals"], dtype=float
+        )
+
+    return proj
+
+
+def test_proj_matrix():
+    """
+    Test projection matrix.
+    """
+    # get parameters from JSON file
+    with open("./parameters.json", "r") as f:
+        parameters = json.load(f)
+
+    ref_proj = read_proj_matrix_from_json("./test028/ref/westpp.json")
+    test_proj = read_proj_matrix_from_json("./test028/test.westpp.save/westpp.json")
+
+    for iexc in ref_proj:
+        np.testing.assert_almost_equal(
+            np.abs(ref_proj[iexc]),
+            np.abs(test_proj[iexc]),
+            decimal=-np.log10(float(parameters["tolerance"]["localization"])),
+        )
